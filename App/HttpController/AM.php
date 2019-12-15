1beffcc99be3d1e0b5327299c274594eb972c7cf
hdtopku <1450948930@qq.com>
2019-12-15 15:17:57 +0800

    applemusic:后端

--- App/HttpController/AM.php
+++ App/HttpController/AM.php
@@ -78,7 +78,7 @@ class AM extends Controller
         $data['operator'] = Operator::create()->all();
         if (array_key_exists('used', $data)) {
             $data['usedLength'] = count($data['used']);
-            $data['used'] = array_splice($data['used'], 0, 50);
+            $data['used'] = array_splice($data['used'], 0, 30);
         }
         $this->response()->write(json_encode(
             ['errno' => '0', 'errmsg' => 'ok', 'data' => $data],
