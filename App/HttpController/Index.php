f545ff7562248698b2f78646b50a87c7ba1e28b4
hdtopku <1450948930@qq.com>
2020-01-16 19:05:14 +0800

    easyswoole:jet随机限时链接

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -22,7 +22,7 @@ class Index extends Controller
     {
         $this->response()->withStatus(404);
         $file = EASYSWOOLE_ROOT.'/vendor/easyswoole/easyswoole/src/Resource/Http/404.html';
-        if(!is_file($file)){
+        if(!is_file($file)) {
             $file = EASYSWOOLE_ROOT.'/src/Resource/Http/404.html';
         }
         $this->response()->write(file_get_contents($file));
---
+++ App/Model/AM/Idea.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lxh
+ * Date: 2020-01-09
+ * Time: 01:07
+ */
+
+namespace App\Model\AM;
+
+
+use EasySwoole\ORM\AbstractModel;
+
+class Idea extends AbstractModel
+{
+    protected $tableName = 'idea';
+}
