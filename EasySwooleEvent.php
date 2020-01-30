1809d7fdf17ee0c5bc1e7ec44bd0a35e1ad6912e
hdtopku <1450948930@qq.com>
2020-01-30 18:48:51 +0800

    easyswoole:批量复制长链

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -65,10 +65,10 @@ class EasySwooleEvent implements Event
         $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         $response->withHeader('Access-Control-Allow-Credentials', 'true');
         $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        if ($request->getMethod() === 'OPTIONS') {
-            $response->withStatus(Status::CODE_OK);
-            return false;
-        }
+//        if ($request->getMethod() === 'OPTIONS') {
+//            $response->withStatus(Status::CODE_OK);
+//            return false;
+//        }
         return true;
     }
 
