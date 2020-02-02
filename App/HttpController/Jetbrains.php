e957d9c2fb4fd9ca3efdf9409ac3ec7ea84132b6
hdtopku <1450948930@qq.com>
2020-02-02 16:18:57 +0800

    edu: 激活码链接

--- App/HttpController/Jetbrains.php
+++ App/HttpController/Jetbrains.php
@@ -86,7 +86,7 @@ class Jetbrains extends Controller
 
     function isValid($data)
     {
-        $validTime = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $validTime = date('Y-m-d H:i:s', strtotime('-8 minutes'));
         if ($data['status'] == 0 || ($data['status'] == 1 && $data['update_time'] >= $validTime)) {
             return true;
         }
