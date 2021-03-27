bc04bd879e575357a89ed38f70eb766f5b92d2d6
hdtopku <1450948930@qq.com>
2021-03-27 23:39:39 +0800

    自动描述：2021-03-27-23:39:39

--- App/HttpController/Router.php
+++ App/HttpController/Router.php
@@ -19,7 +19,7 @@ class Router extends AbstractRouter
     function initialize(RouteCollector $routeCollector)
     {
         $routeCollector->get('/user', '/index.html');
-        $routeCollector->get('/nfsmabeawn', '/AM/get');
+        $routeCollector->get('/am/nfsmabeawn', '/AM/get');
         $routeCollector->get('/am/rdata', '/RandomData');
         $routeCollector->get('/am/j', '/Jetbrains');
         $routeCollector->addRoute(['GET', 'POST', 'OPTIONS'], '/am/jc', '/Jetbrains/activeCode');
